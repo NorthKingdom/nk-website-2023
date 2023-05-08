56ffd71e658c3fde87ec4a9aa2bb72ded9a82a0c
Nick White <nicholas@northkingdom.com>
2023-05-08 13:03:06 +0200

    add: enivronment on the graphql query

--- src/utils/contentful.ts
+++ src/utils/contentful.ts
@@ -1,14 +1,17 @@
 const queryContentful = async (_query: string) => {
-  return fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CTF_SPACE_TOKEN}`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      authorization: `Bearer ${process.env.NEXT_PUBLIC_CTF_ACCESS_TOKEN}`,
-    },
-    body: JSON.stringify({
-      query: _query,
-    }),
-  })
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CTF_SPACE_TOKEN}/environments/${process.env.NEXT_PUBLIC_CTF_ENVIRONMENT}`,
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${process.env.NEXT_PUBLIC_CTF_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query: _query,
+      }),
+    }
+  )
     .then((res) => res.json())
     .then((queryData) => {
       return queryData.data
