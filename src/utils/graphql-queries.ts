7d2e409d60d1d064582e7752387aae6ae490aff7
mklasinc <miha.klasinc@gmail.com>
2023-05-10 16:17:37 +0200

    feat: aspect ratio

--- src/utils/graphql-queries.ts
+++ src/utils/graphql-queries.ts
@@ -1,4 +1,24 @@
 export const HOME_PAGE_QUERY = `
+
+
+fragment video on Video {
+    srcCollection {
+      items {
+        url
+      }
+    }
+  }
+
+fragment responsiveImage on ResponsiveImage {
+    desktopImage {
+      url
+    }
+    mobileImage {
+      url
+    }
+    altText
+  }
+
 {
     home(id: "3uX3aK4XeCQySQQohsAsyN") {
         aboutNk
@@ -8,6 +28,11 @@ export const HOME_PAGE_QUERY = `
                 client
                 vertical
                 slug
+                description
+                thumbnail {
+                    ...responsiveImage
+                    ...video
+                }
             }
         }
         journalSectionText
