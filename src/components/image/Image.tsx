f36806a440f695d2287ec8f71cbe4560628e01c4
Nick White <nicholas@northkingdom.com>
2023-05-12 15:17:25 +0200

    update: background case color and some keys

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import styles from './Image.module.scss'
+import { motion } from 'framer-motion'
 import { bemify } from '@utils/bemify'
 const bem = bemify(styles, 'image')
 
 interface ImageProps {
+  className?: string
   srcSet: string[]
   alt: string
   caption?: string
 }
 
-export const Image = ({ srcSet, alt, caption }: ImageProps) => {
+export const Image = ({ className, srcSet, alt, caption }: ImageProps) => {
   {
     /* TODO :: Can change to next/image component */
   }
   return (
     <>
-      <picture className={styles['image']}>
+      <picture className={`${className} ${styles['image']}`}>
         <source srcSet={srcSet[0]} />
         <img srcSet={srcSet[0]} alt={alt} />
       </picture>
