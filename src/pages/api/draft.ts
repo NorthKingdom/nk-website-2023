52be645688eda4b07e1286da45807f83604f0bd9
Nick White <nicholas@northkingdom.com>
2023-05-15 15:18:10 +0200

    update: draft queries for different pages

--- src/pages/api/draft.ts
+++ src/pages/api/draft.ts
@@ -1,5 +1,5 @@
 import client from '@graphql/client'
-import { CASE_PAGE_QUERY } from '../../graphql/queries'
+import { CASE_PAGE_QUERY, HOME_PAGE_QUERY, ABOUT_PAGE_QUERY, WORK_PAGE_QUERY } from '../../graphql/queries'
 
 // simple example for testing it manually from your browser.
 export default async function handler(req: any, res: any) {
@@ -9,17 +9,61 @@ export default async function handler(req: any, res: any) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // Fetch the headless CMS to check if the provided `slug` exists
-  // getPostBySlug would implement the required fetching logic to the headless CMS
+  const routeParams = req.query.slug.split('/')
+  const contentType = routeParams[1] === '' ? 'home' : routeParams[1]
 
-  const post = await client(true)
-    .query({
-      query: CASE_PAGE_QUERY(req.query.slug, true),
-    })
-    .then((res) => res.data)
-    .then((data) => {
-      return { props: data.caseCollection.items[0] }
-    })
+  let post = null
+  let url = null
+
+  switch (contentType) {
+    case 'home':
+      post = await client(true)
+        .query({
+          query: HOME_PAGE_QUERY(true),
+        })
+        .then((res: any) => res.data)
+        .then((data: any) => {
+          return { props: data.home }
+        })
+      url = `/`
+      break
+    case 'case':
+      post = await client(true)
+        .query({
+          query: CASE_PAGE_QUERY(req.query.slug, true),
+        })
+        .then((res) => res.data)
+        .then((data) => {
+          return { props: data.caseCollection.items[0] }
+        })
+      // @ts-ignore
+      url = `/case/${post.props.slug}`
+      break
+    case 'about':
+      post = await client(true)
+        .query({
+          query: ABOUT_PAGE_QUERY(true),
+        })
+        .then((res: any) => res.data)
+        .then((data: any) => {
+          return { props: data.about }
+        })
+      url = `/about`
+      break
+    case 'work':
+      post = await client(true)
+        .query({
+          query: WORK_PAGE_QUERY(true),
+        })
+        .then((res: any) => res.data)
+        .then((data: any) => {
+          return { props: data }
+        })
+      url = `/work`
+      break
+    default:
+      console.log(`CANT FIND DATA IN CONTENTFUL FOR TYPE: ${contentType}`)
+  }
 
   // If the slug doesn't exist prevent draft mode from being enabled
   if (!post) {
@@ -32,5 +76,5 @@ export default async function handler(req: any, res: any) {
   // Redirect to the path from the fetched post
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
   // @ts-ignore
-  res.redirect(`/case/${post.props.slug}`)
+  res.redirect(url)
 }
