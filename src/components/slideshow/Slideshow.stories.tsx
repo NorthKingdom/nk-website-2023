a15299d6e1576cc9066e430300bb3545089fd7fb
Nick White <nicholas@northkingdom.com>
2023-05-15 12:47:40 +0200

    update: slideshow with captions

--- src/components/slideshow/Slideshow.stories.tsx
+++ src/components/slideshow/Slideshow.stories.tsx
@@ -1,14 +1,85 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Slideshow } from './Slideshow';
+import type { Meta, StoryObj } from '@storybook/react'
+import { Slideshow } from './Slideshow'
 
 const meta: Meta<typeof Slideshow> = {
   title: 'UI/Slideshow',
   component: Slideshow,
   tags: ['autodocs'],
-  parameters: {}
-};
+  parameters: {},
+}
 
-export default meta;
-type Story = StoryObj<typeof Slideshow>;
+export default meta
+type Story = StoryObj<typeof Slideshow>
 
-export const Default: Story = { args: {} };
+const srcSet = [
+  {
+    desktopImage: {
+      url: '/dummy/temp-left-riot-img.jpg',
+    },
+    mobileImage: {
+      url: '/dummy/temp-left-riot-img.jpg',
+    },
+    altText: 'temp alt',
+    imageCaption: 'temp caption 1',
+  },
+  {
+    desktopImage: {
+      url: '/dummy/temp-right-riot-img.jpg',
+    },
+    mobileImage: {
+      url: '/dummy/temp-right-riot-img.jpg',
+    },
+    altText: 'temp alt',
+    imageCaption: 'temp caption 2',
+  },
+  {
+    desktopImage: {
+      url: '/dummy/temp-riot-right-image-2.jpg',
+    },
+    mobileImage: {
+      url: '/dummy/temp-riot-right-image-2.jpg',
+    },
+    altText: 'temp alt',
+    imageCaption: 'temp caption 3',
+  },
+]
+
+export const Default: Story = {
+  args: {
+    automaticallyChange: true,
+    showIndicators: false,
+    showArrows: false,
+    showCaption: false,
+    srcSet: srcSet,
+  },
+}
+
+export const WithCaptions: Story = {
+  args: {
+    automaticallyChange: true,
+    showIndicators: false,
+    showArrows: false,
+    showCaption: true,
+    srcSet: srcSet,
+  },
+}
+
+export const CanManuallyChangeSlides: Story = {
+  args: {
+    automaticallyChange: true,
+    showIndicators: true,
+    showArrows: true,
+    showCaption: false,
+    srcSet: srcSet,
+  },
+}
+
+export const CanChangeSlidesWithCaptions: Story = {
+  args: {
+    automaticallyChange: true,
+    showIndicators: true,
+    showArrows: true,
+    showCaption: true,
+    srcSet: srcSet,
+  },
+}
