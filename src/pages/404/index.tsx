961d5d2bd7cfa8382f09786d62e272e07bb9ada2
mklasinc <miha.klasinc@gmail.com>
2023-05-29 16:38:49 +0200

    feat(error-page): added custom 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,20 +1,50 @@
 import React from 'react'
 import Head from 'next/head'
-import Link from 'next/link'
-import style from './404.module.scss'
+import { gql } from '@apollo/client'
+import client from '@graphql/client'
+import { RESPONSIVE_IMAGE } from '@graphql/fragments/ResponsiveImage.fragment'
+import { PageHero } from '@components/page-hero'
+import { ResponsiveImage } from '@customTypes/cms'
 
-const Custom404 = () => {
+interface FourOhFourProps {
+  title: string
+  image: ResponsiveImage
+}
+
+const ERROR_PAGE_QUERY = (draftMode: boolean) => gql`
+  ${RESPONSIVE_IMAGE}
+
+  query ErrorPageQuery {
+    errorPage(id: "1f3waB8hHDqowsAW0PEDpe") {
+      title
+      image {
+        ...responsiveImage
+      }
+    }
+  }
+`
+
+const Custom404 = ({ title, image }: FourOhFourProps) => {
   return (
     <>
       <Head>
         <link rel="canonical" href="https://www.northkingdom.com/404" />
       </Head>
-      <main className={style.container}>
-        <h1>Not found</h1>
-        <Link href="/">Back</Link>
-      </main>
+      <PageHero title={title} srcSet={image} />
     </>
   )
+}
+export async function getStaticProps({ draftMode = false }) {
+  try {
+    const res = await client(draftMode).query({
+      query: ERROR_PAGE_QUERY(draftMode),
+    })
+
+    return { props: res.data.errorPage }
+  } catch (error) {
+    console.error(error)
+    return { props: { title: '404', image: null } }
+  }
 }
 
 export default Custom404
