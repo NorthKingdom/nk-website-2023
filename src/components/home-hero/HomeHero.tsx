abff69031835257e6eaa362fd2d78eccb89b80a9
mklasinc <miha.klasinc@gmail.com>
2023-06-01 12:30:43 +0200

    feat: add fake image to improve LCP

--- src/components/home-hero/HomeHero.tsx
+++ src/components/home-hero/HomeHero.tsx
@@ -73,6 +73,17 @@ export const HomeHero = ({ statement, showreelVideo, shieldVideo }: HomeHeroProp
           eventPrefix="client"
         />
 
+        {/* image to improve LCP */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          aria-hidden={true}
+          alt=""
+          className={bem('lcp')}
+          width={99999}
+          height={99999}
+          src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c3ZnIHdpZHRoPSI5OTk5OXB4IiBoZWlnaHQ9Ijk5OTk5cHgiIHZpZXdCb3g9IjAgMCA5OTk5OSA5OTk5OSIgdmVyc2lvbj0iMS4xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48ZyBzdHJva2U9Im5vbmUiIGZpbGw9Im5vbmUiIGZpbGwtb3BhY2l0eT0iMCI+PHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9Ijk5OTk5IiBoZWlnaHQ9Ijk5OTk5Ij48L3JlY3Q+IDwvZz4gPC9zdmc+"
+        />
+
         <div aria-hidden="true" className={bem('overlay')} data-visible={!loaded} />
 
         <h1 className={bem('title')} aria-label="North Kingdom">
