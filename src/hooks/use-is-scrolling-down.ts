74baea208898d05ece304e644d496aac46fe4e4f
mklasinc <miha.klasinc@gmail.com>
2023-06-20 11:07:33 +0200

    fix(use-is-scrolling-down): add minimum scroll distance threshold

--- src/hooks/use-is-scrolling-down.ts
+++ src/hooks/use-is-scrolling-down.ts
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import { useOnScroll } from './use-on-scroll'
 
-export const useIsScrollingDown = () => {
+/**
+ * Check if user is scrolling down
+ * @param threshold Minimum scroll distance to consider scrolling down
+ * @returns {boolean} Whether user is scrolling down
+ */
+export const useIsScrollingDown = (threshold = 30) => {
   const [isScrollingDown, setIsScrollingDown] = useState(false)
 
-  useOnScroll(({ direction }) => {
+  useOnScroll(({ direction, scroll }) => {
+    if (scroll < threshold && isScrollingDown) setIsScrollingDown(false)
+
     if (direction === 1 && !isScrollingDown) {
       setIsScrollingDown(true)
     } else if (direction === -1 && isScrollingDown) {
