474ab87892f791d3bf156a3e3fe3f847ab71f2e0
mklasinc <miha.klasinc@gmail.com>
2023-06-26 15:55:36 +0200

    feat(page-hero): animate page hero image

--- src/components/page-hero/PageHero.tsx
+++ src/components/page-hero/PageHero.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 import Link from 'next/link'
+import cx from 'clsx'
 import { bemify } from '@utils/bemify'
 import { Media } from '@components/media'
 import { ContentWrapper } from '@components/content-wrapper/ContentWrapper'
 import styles from './PageHero.module.scss'
 import type { PageHeroPayload } from '@customTypes/cms'
 const bem = bemify(styles, 'pageHero')
+const pageHeroImageBem = bemify(styles, 'pageHeroImage')
 
 interface PageHeroProps extends Omit<PageHeroPayload, '__typename'> {
   notch?: boolean
@@ -13,6 +15,10 @@ interface PageHeroProps extends Omit<PageHeroPayload, '__typename'> {
 }
 
 export const PageHero = ({ title, image, className, link, notch = false }: PageHeroProps) => {
+  const [showHeroImage, toggle] = useReducer(() => true, false)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(toggle, [])
+
   return (
     <ContentWrapper fullscreen theme="dark" className={`${styles['pageHero']} ${className}`} notch={notch}>
       <ContentWrapper className={bem('titleContainer')} theme="dark">
@@ -24,10 +30,26 @@ export const PageHero = ({ title, image, className, link, notch = false }: PageH
         )}
       </ContentWrapper>
       {image && (
-        <div className={bem('imageContainer')}>
-          <Media {...image} className={bem('image')} />
+        <div className={cx(bem('imageContainer'), pageHeroImageBem())} data-show={showHeroImage}>
+          <div className={pageHeroImageBem('inner')}>
+            <div className={pageHeroImageBem('scale')}>
+              <Media {...image} className={bem('image')} />
+            </div>
+          </div>
         </div>
       )}
     </ContentWrapper>
   )
 }
+
+// const ImageRevealWipe = ({ image }) => {
+//   return (
+//     <div className={cx(styles.wipeRevealImage, styles.container)} data-show={show}>
+//       <div className={styles.inner}>
+//         <div className={styles.scale}>
+//           <Media {...image} className={bem('image')} />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
